f2eec879763804a18efcd8f9e934797684d9f40a
sangjun1126 <jeongsangjun1234@gmail.com>
2024-01-31 15:53:55 +0900

    second commit

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# layout.js는 page.js를 감싸는 파일입니다.
+## 코딩애플 next.js 13버전 설명
+
+- layout.js는 page.js를 감싸는 파일입니다.
+- globals.css 전체 css임 (index.css 느낌)
+- xxx.module.css은 특정 페이지에만 적용 가능한 css입니다.
--- src/app/page.js
+++ src/app/page.js
@@ -1,95 +1,17 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+// 리액트 jsx 문법을 지켜줘야 함
+// 중괄호 쓰면 data binding이 발생함
 
 export default function Home() {
+  let name = "정상준";
+  let title_name = "Apple_fresh";
+  let link = "http://google.com";
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+    <div>
+      <h4 className="title">{title_name}</h4>
+      <p className="title-sub">by dev {name}</p>
+      <a className="link " href={link}>
+        링크
+      </a>
+    </div>
   );
 }
